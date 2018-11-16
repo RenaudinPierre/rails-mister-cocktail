37e507a1212e0bf857a134eed66be59ee98e1f21
Pierre Renaudin <renaudin.pierre@gmail.com>
2018-11-16 11:51:38 +0100

    dddd

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+  # def new
+  #   @dose = Dose.new
+  # end
+
+  def create
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @doses = @cocktail.doses
+      render 'cocktails#show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy!
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
